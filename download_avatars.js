cf4081bfd97fcf0f60d367e22e1f073e4456341b
kwan14 <mendel.kwan@telus.net>
2017-09-12 03:32:10 +0000

    Tidied code

--- download_avatars.js
+++ download_avatars.js
@@ -4,9 +4,13 @@ var fs = require("fs");
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// Please replace these values with a valid GitHub user and token
+
 var GITHUB_USER = "kwan14";
 var GITHUB_TOKEN = "9aacaec0a02e03f944dfb9bec1536e94ac87659b";
 
+var owner = process.argv[2];
+var name = process.argv[3];
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
@@ -21,17 +25,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
   })
 }
 
-getRepoContributors("jquery", "jquery", (err, result) => {
-  console.log("Errors:", err);
-  result.forEach((currentValue) => {
-    downloadImageByURL(currentValue.avatar_url, "avatars/" + currentValue.login + ".jpg");
-  })
-})
-
-
-
 function downloadImageByURL(url, filepath) {
   request.get(url).pipe(fs.createWriteStream(filepath));
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+if(owner === undefined || name === undefined) {
+  console.log("Usage: node download_avatar <repository_owner> <repository_name>");
+} else {
+  getRepoContributors(owner, name, (err, result) => {
+    result.forEach((currentValue) => {
+      downloadImageByURL(currentValue.avatar_url, "avatars/" + currentValue.login + ".jpg");
+    })
+  })
+}
+
+
+
+
