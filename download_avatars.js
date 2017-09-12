5ebd6a01804c1ce1cc0bb5448f1953d17af68213
kwan14 <mendel.kwan@telus.net>
2017-09-12 02:15:22 +0000

    Made request to GitHub API for contributor list

--- download_avatars.js
+++ download_avatars.js
@@ -3,8 +3,21 @@ var request = require("request");
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+var GITHUB_USER = "kwan14";
+var GITHUB_TOKEN = "9aacaec0a02e03f944dfb9bec1536e94ac87659b";
 
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  var options = {
+    url : requestURL,
+    headers : {
+      "User-Agent" : "request"
+    }
+  };
+  request(options, (error, response, body) => {
+    console.log(body);
+  })
 }
 
 getRepoContributors("jquery", "jquery", (err, result) => {
